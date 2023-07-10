0f23e196a5dd563544e43217da30fa32ff4fff73
Shobit51 <138685037+Shobit51@users.noreply.github.com>
2023-07-10 14:58:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config:
     BOT_SESSION = os.environ.get("BOT_SESSION", "forwardbot")
     OWNER_ID = os.environ.get("OWNER_ID", "")                             
     DATABASE_URI = os.environ.get("")  
-    DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
+    DATABASE_NAME = os.environ.get("DATABASE_NAME", "Forward")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Forward_data')
     SESSION = os.environ.get("SESSION", "")   
     TO_CHANNEL = int(os.environ.get("TO_CHANNEL", ""))
